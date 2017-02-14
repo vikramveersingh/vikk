93b5fb66a6cbb2d6e4f77ba02caf5e7d2d508e24
vikramveersingh <vikramveer11295@gmail.com>
2017-02-14 17:02:11 +0545

    Add files via upload

---
+++ applet2.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import java.applet.*;
+/*<applet code="applet2" width=60 height=40>
+</applet>*/
+public class applet2 extends Applet{
+public void paint(Graphics g){
+	g.drawString("this is my first applet",30,30);
+	}
+	}
